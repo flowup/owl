aa740df8e0ddf67ab74a8eda5e46b3112d8af52d
rysavyvladan <rysavy.vladan@gmail.com>
2017-01-09 14:55:43 +0100

    feat(cli) integrated cli (fix #2, fix #3)

--- cmd/owl/main.go
+++ cmd/owl/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/flowup/owl/.glide/cache/src/https-github.com-urfave-cli"
 	"fmt"
-	"path/filepath"
 	"os"
+	"strings"
+	"path/filepath"
 	"log"
 	"regexp"
 	"os/exec"
@@ -11,80 +13,111 @@ import (
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "owl"
+	app.Usage = "owl watching all files in directory and when are changed, run the command"
 
-	// set new watcher
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		panic(err)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "ignore, i",
+			Usage:"All directories with name `IGNORE` are ignored",
+		},
+		cli.StringFlag{
+			Name: "run, r",
+			Usage:"If is any file changed, run `RUN`",
+		},
 	}
 
-	// get path to this dir
-	path, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	// append path to global paths
-	dirList := []string{}
-
-	err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+	app.Action = func(c *cli.Context) error {
+		// set new watcher
+		watcher, err := fsnotify.NewWatcher()
 		if err != nil {
-			log.Print(err)
-			return nil
+			panic(err)
 		}
+
+		// get path to this dir
+		path, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		// append path to global paths
+		dirList := []string{}
 
 		// this files are ignore
-		ignoreList := []string{"vendor", ".glide", ".git"}
+		ignoreList := make(map[string]bool)
+		for _, dir := range (strings.Split(c.String("ignore"), ";")) {
+			ignoreList[dir] = true
+		}
 
-		// check if file is not in ignorelist
-		for _, i := range ignoreList {
-			if info.Name() == i {
+		err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Print(err)
+				return nil
+			}
+
+			// check if file is not in ignorelist
+			if ignoreList[info.Name()] {
 				return filepath.SkipDir
 			}
-		}
 
-		// append dir to list
-		if info.IsDir() {
-			dirList = append(dirList, path)
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// add all paths for watching
-	for _, path := range dirList {
-		watcher.Add(path)
-	}
-
-	// seperate argument into two parts
-	re := regexp.MustCompile("^([A-Za-z0-9]+)\\s?(.*)")
-	groups := re.FindStringSubmatch(os.Args[1:][0])
-
-	// name of function
-	name := groups[1]
-	// his arguments
-	arq := groups[2]
-
-	for {
-		select {
-		case ev := <-watcher.Events:
-			// Write is running only once
-			if ev.Op == fsnotify.Write {
-				// execute of function with arguments
-				output, err := exec.Command(name, arq).CombinedOutput()
-				if err != nil {
-					os.Stderr.WriteString(err.Error())
-				}
-				fmt.Print(string(output))
-
+			// append dir to list
+			if info.IsDir() {
+				dirList = append(dirList, path)
 			}
+			return nil
+		})
 
-
-		case err := <-watcher.Errors:
-			log.Println("error:", err)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		// add all paths for watching
+		for _, path := range dirList {
+			watcher.Add(path)
+		}
+
+		// seperate argument into two parts
+		fmt.Println(c.String("run"))
+
+		re := regexp.MustCompile("^([A-Za-z0-9]+)\\s?(.*)")
+
+		var name []string
+		var arq []string
+
+		for _, i := range strings.Split(c.String("run"), ";") {
+			groups := re.FindStringSubmatch(i)
+
+			// name of function
+			name = append(name, groups[1])
+			// his arguments
+			arq = append(arq, groups[2])
+		}
+
+		for {
+			select {
+			case ev := <-watcher.Events:
+				// Write is running only once
+				if ev.Op == fsnotify.Write {
+					// execute of function with arguments
+					for x, _ := range name {
+						output, err := exec.Command(name[x], arq[x]).CombinedOutput()
+						if err != nil {
+							os.Stderr.WriteString(err.Error())
+						}
+						fmt.Print(string(output))
+					}
+
+				}
+
+
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+
+		return nil
 	}
+
+	app.Run(os.Args)
 
 }
