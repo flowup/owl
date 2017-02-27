4a9e2f4a33f6eca888245986ef8c80b97e9c0576
Jakub Kulich <jakubkulich@gmail.com>
2017-02-27 13:14:21 +0100

    refactor(owl): config reading moved out of the owl

--- cmd/owl/main.go
+++ cmd/owl/main.go
@@ -4,8 +4,17 @@ import (
 	"os"
 	"github.com/urfave/cli"
 	"github.com/flowup/owl"
+	"github.com/spf13/viper"
+	"regexp"
+	"strconv"
+	"errors"
+	"go.uber.org/zap"
 )
 
+var (
+	errRunFlagMissing = errors.New("flag --run or -r is required ")
+	defaultIgnored    = [...] string{"vendor", "node_modules", "bower_components", ".glide", ".git"}
+)
 
 func main() {
 
@@ -13,9 +22,55 @@ func main() {
 
 	o.App.Action = func(c *cli.Context) error {
 
-		err := o.ReadConfigAndInit(c)
+		o.Config.SetConfigType("yaml")
+		o.Config.SetConfigName("owl")
+		o.Config.AddConfigPath(".")
+
+		err := o.Config.ReadInConfig()
+		o.Config.SetDefault("debounce", 500)
+		o.Config.SetDefault("verbose", false)
+		o.Config.SetDefault("ignore", make([]string, 0))
+
+		// If no config is present in current folder, read options from args
 		if err != nil {
-			return err
+			o.Config.Set("run", c.String("run"))
+			if c.Bool("v") {
+				o.Config.Set("verbose", true)
+			}
+			if c.String("d") != "" {
+				debounce, err := strconv.ParseInt(c.String("d"), 10, 64)
+				if err != nil {
+					panic(err)
+				}
+				o.Config.Set("debounce", debounce)
+			}
+			o.Config.Set("ignore", c.StringSlice("ignore"))
+			o.Config.Set("filter", c.StringSlice("filter"))
+		}
+
+		if o.Config.GetString("run") == "" {
+			return errRunFlagMissing
+		}
+
+		if o.Config.GetBool("verbose") {
+			o.Logger = owl.GetLogger(zap.InfoLevel)
+		} else {
+			o.Logger = owl.GetLogger(zap.WarnLevel)
+		}
+
+		// job dir are ignored by default
+		o.IgnoreList = make(map[string]bool)
+
+		for _, dir := range defaultIgnored {
+			o.IgnoreList[dir] = true
+		}
+
+		for _, dir := range viper.GetStringSlice("ignore") {
+			o.IgnoreList[dir] = true
+		}
+
+		if o.Config.GetString("filter") != "" {
+			o.FilterRegexp = regexp.MustCompile(o.Config.GetString("filter"))
 		}
 
 		err = o.SetupWatcher()
--- owl.go
+++ owl.go
@@ -3,8 +3,6 @@ package owl
 import (
 	"github.com/spf13/viper"
 	"github.com/urfave/cli"
-	"errors"
-	"strconv"
 	"go.uber.org/zap"
 	"regexp"
 	"github.com/fsnotify/fsnotify"
@@ -13,90 +11,24 @@ import (
 	"fmt"
 )
 
-var (
-	errRunFlagMissing = errors.New("flag --run or -r is required ")
-	defaultIgnored    = [...] string{"vendor", "node_modules", "bower_components", ".glide", ".git"}
-)
-
 // Owl is tool for advanced watching of changes in directories and files
 type Owl struct {
 	App    *cli.App
 	Logger *zap.Logger
 
-	config       *viper.Viper
-	watcher      *fsnotify.Watcher
-	filterRegexp *regexp.Regexp
-	ignoreList   map[string]bool
+	Config       *viper.Viper
+	Watcher      *fsnotify.Watcher
+	FilterRegexp *regexp.Regexp
+	IgnoreList   map[string]bool
 }
 
 // NewOwl is function that creates new instance of the Owl
 func NewOwl() *Owl {
 	return &Owl{
-		config:     viper.New(),
+		Config:     viper.New(),
 		App:        GetApp(),
-		ignoreList: make(map[string]bool),
+		IgnoreList: make(map[string]bool),
 	}
-}
-
-// ReadConfigAndInit reads config from configuration file and if configuration file is not present then configuration
-// is read from program arguments.
-func (o *Owl) ReadConfigAndInit(c *cli.Context) error {
-
-	o.config.SetConfigType("yaml")
-	o.config.SetConfigName("owl")
-	o.config.AddConfigPath(".")
-
-	err := o.config.ReadInConfig()
-	o.config.SetDefault("debounce", 500)
-	o.config.SetDefault("verbose", false)
-	o.config.SetDefault("ignore", make([]string, 0))
-
-	// If no config is present in current folder, read options from args
-	if err != nil {
-		o.config.Set("run", c.String("run"))
-		if c.Bool("v") {
-			o.config.Set("verbose", true)
-		}
-		if c.String("d") != "" {
-			debounce, err := strconv.ParseInt(c.String("d"), 10, 64)
-			if err != nil {
-				panic(err)
-			}
-			o.config.Set("debounce", debounce)
-		}
-		o.config.Set("ignore", c.StringSlice("ignore"))
-		o.config.Set("filter", c.StringSlice("filter"))
-	}
-
-	if o.config.GetString("run") == "" {
-		return errRunFlagMissing
-	}
-
-	if o.config.GetBool("verbose") {
-		o.Logger = GetLogger(zap.InfoLevel)
-	} else {
-		o.Logger = GetLogger(zap.WarnLevel)
-	}
-
-	// job dir are ignored by default
-	o.ignoreList = make(map[string]bool)
-
-	for _, dir := range defaultIgnored {
-		o.ignoreList[dir] = true
-	}
-
-	for _, dir := range viper.GetStringSlice("ignore") {
-		o.ignoreList[dir] = true
-	}
-
-	if o.config.GetString("filter") != "" {
-		o.filterRegexp = regexp.MustCompile(o.config.GetString("filter"))
-	}
-
-	// set new watcher
-	o.watcher, err = fsnotify.NewWatcher()
-
-	return err
 }
 
 // SetupWatcher is function for setting watcher's directories
@@ -108,18 +40,24 @@ func (o *Owl) SetupWatcher() error {
 		panic(err)
 	}
 
+	// set new watcher
+	o.Watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		panic(err)
+	}
+
 	err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
 		// check if dir is not in ignorelist
-		if o.ignoreList[info.Name()] && info.IsDir() {
+		if o.IgnoreList[info.Name()] && info.IsDir() {
 			return filepath.SkipDir
 		}
 
 		if info.IsDir() {
-			o.watcher.Add(path)
+			o.Watcher.Add(path)
 		}
 
 		return nil
@@ -135,7 +73,7 @@ func (o *Owl) Watch() error {
 
 	// init job
 	watcherJob := &WatcherJob{
-		Command: o.config.GetString("run"),
+		Command: o.Config.GetString("run"),
 		OutPipe: os.Stdout}
 
 	// start the command at the start of the owl
@@ -144,18 +82,18 @@ func (o *Owl) Watch() error {
 	go func() {
 		for {
 			select {
-			case ev := <-o.watcher.Events:
+			case ev := <-o.Watcher.Events:
 
 				// check if is set filter
-				if o.filterRegexp != nil {
-					if !o.filterRegexp.MatchString(ev.Name) {
+				if o.FilterRegexp != nil {
+					if !o.FilterRegexp.MatchString(ev.Name) {
 						break
 					}
 				}
 
 				// Write is running only once
 				if ev.Op == fsnotify.Create {
-					o.watcher.Add(ev.Name)
+					o.Watcher.Add(ev.Name)
 				}
 
 				// Log event
@@ -164,13 +102,13 @@ func (o *Owl) Watch() error {
 				// Add Job to jobs
 				jobs <- watcherJob
 
-			case err := <-o.watcher.Errors:
+			case err := <-o.Watcher.Errors:
 				o.Logger.Fatal(err.Error())
 			}
 		}
 	}()
 
-	dJobs := Debounce(jobs, o.config.GetInt64("debounce"))
+	dJobs := Debounce(jobs, o.Config.GetInt64("debounce"))
 	results := Scheduler(dJobs)
 
 	for {
